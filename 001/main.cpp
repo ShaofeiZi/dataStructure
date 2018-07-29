b3568d8a15140ef8b3c019820f4637edd307f5dd
訾绍飞 <zishaofei221@gmail.com>
2018-07-29 22:45:46 +0800

    helloword 了解一下  输入输出了解下

---
+++ 001/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+//  string name;
+//  cin >> name;
+//  cout << "hello, " << name << endl;
+//  cout << name.size() << endl;
+//  name = name + name;
+//  cout << name << endl;
+//    int a, b;
+//    cin >> a >> b;
+//    cout << "a+b=" << a + b;
+    cout << "hello world" << endl;
+    return 0;
+}
+
+
