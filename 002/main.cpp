288ece1d0ddb4b35610dfc90eb2f36c989a40438
訾绍飞 <zishaofei221@gmail.com>
2018-07-30 00:21:09 +0800

    数组 向量

--- 002/main.cpp
+++ 002/main.cpp
@@ -3,19 +3,14 @@
 
 using namespace std;
 
-int main() {
-  string name;
-  cout << "请输入姓名" << endl; 
-  cin >> name;
-  cout << "hello, " << name << endl;
-  cout << "长度为" << name.size() << endl;
-  name = name + name;
-//  cout << name << endl;
-//    int a, b;
-//    cin >> a >> b;
-//    cout << "a+b=" << a + b;
-
-    return 0;
+int main()
+{
+	string name;
+	cin >> name;
+	cout << "hello, " << name << endl;
+	cout << name.size() << endl;
+	name = name + name;
+	cout << name << endl;
+	system("pause");
+	return 0;
 }
-
-
